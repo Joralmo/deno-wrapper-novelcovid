5f8ca237ad72c9a67b403d44a9aae421403f155b
JoralmoPro <JoralmoPro@gmail.com>
2020-05-14 21:16:17 -0500

    corrigiendo endpoint de continentes 😎

--- src/helpers/Endpoints.ts
+++ src/helpers/Endpoints.ts
@@ -3,7 +3,7 @@ import { Options } from "./../../src/Client.ts";
 export const Endpoints = {
     COUNTRIES: (opts?: Options.country) => `/v2/countries?sort=${opts?.sort}&yesterday=${opts?.yesterday}`,
     COUNTRY: (country: string) => `/v2/countries/${country}`,
-    CONTINENTS: (opts?: Options.continents) => `/v2/countries?sort=${opts?.sort}`,
+    CONTINENTS: (opts?: Options.continents) => `/v2/continents?sort=${opts?.sort}`,
     CONTINENT: (continent: string) => `/v2/continents/${continent}`,
     STATES: (opts?: Options.states) => `/v2/states?sort=${opts?.sort}`,
     STATE: (state: string) => `/v2/states/${state}`,
